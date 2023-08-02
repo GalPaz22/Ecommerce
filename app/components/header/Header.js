0274291cb5f15ce3936baa18b01007f2cc06d346
GalPaz <galpaz2210@gmail.com>
2023-08-02 22:58:05 +0300

    create header

---
+++ app/components/header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className='w-full h-14 bg-primary'>
+        
+    </div>
+  )
+}
---
+++ app/components/header/Logo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Logo = () => {
+  return (
+    <img  src='"C:\Users\user\Desktop\Wine-Logo.png"'></img>
+  )
+}
